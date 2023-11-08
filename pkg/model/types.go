cf0873bf22103cfb35f64121a0d40837072d83ee
梧桐 <aidecloud@163.com>
2023-11-08 10:45:10 +0800

    增加业务字典类型枚举

--- pkg/model/types.go
+++ pkg/model/types.go
@@ -4,9 +4,29 @@ import (
 	"fmt"
 )
 
+var _ fmt.Stringer = (*Category)(nil)
+var _ fmt.Stringer = (*Status)(nil)
+
 type (
 	Category int
 	Status   int
+)
+
+const (
+	Enable Status = iota + 1
+	// Disable 关闭
+	Disable
+)
+
+const (
+	// CategoryPromLabel 用于标签
+	CategoryPromLabel Category = iota + 1
+	// CategoryPromAnnotation 用于注释
+	CategoryPromAnnotation
+	// CategoryPromStrategy 用于策略
+	CategoryPromStrategy
+	// CategoryPromStrategyGroup 用于策略组
+	CategoryPromStrategyGroup
 )
 
 const unknown = "未知"
@@ -22,18 +42,25 @@ func (s Status) String() string {
 	}
 }
 
+func (s Status) Value() int {
+	return int(s)
+}
+
 func (c Category) String() string {
 	switch c {
+	case CategoryPromLabel:
+		return "标签"
+	case CategoryPromAnnotation:
+		return "注释"
+	case CategoryPromStrategy:
+		return "策略"
+	case CategoryPromStrategyGroup:
+		return "策略组"
 	default:
 		return unknown
 	}
 }
 
-var _ fmt.Stringer = (*Category)(nil)
-var _ fmt.Stringer = (*Status)(nil)
-
-const (
-	Enable Status = iota + 1
-	// Disable 关闭
-	Disable
-)
+func (c Category) Value() int {
+	return int(c)
+}
